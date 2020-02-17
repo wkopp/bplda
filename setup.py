c5e795f4e046a1d0221d57542e693a50de8e784c
Wolfgang Kopp <wolfgang.kopp@mdc-berlin.de>
2020-02-17 22:50:34 +0100

    code black adapted

--- setup.py
+++ setup.py
@@ -30,8 +30,7 @@ except ImportError:
 
 def read(*names, **kwargs):
     with io.open(
-        join(dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8')
+        join(dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")
     ) as fh:
         return fh.read()
 
@@ -39,8 +38,9 @@ def read(*names, **kwargs):
 # Enable code coverage for C code: we can't use CFLAGS=-coverage in tox.ini, since that may mess with compiling
 # dependencies (e.g. numpy). Therefore we set SETUPPY_CFLAGS=-coverage in tox.ini and copy it to CFLAGS here (after
 # deps have been safely installed).
-if 'TOXENV' in os.environ and 'SETUPPY_CFLAGS' in os.environ:
-    os.environ['CFLAGS'] = os.environ['SETUPPY_CFLAGS']
+if "TOXENV" in os.environ and "SETUPPY_CFLAGS" in os.environ:
+    os.environ["CFLAGS"] = os.environ["SETUPPY_CFLAGS"]
+
 
 def is_installed(requirement):
     try:
@@ -50,45 +50,54 @@ def is_installed(requirement):
     else:
         return True
 
-if not is_installed('numpy'):
-    print(textwrap.dedent("""
+
+if not is_installed("numpy"):
+    print(
+        textwrap.dedent(
+            """
             Error: numpy needs to be installed first. You can install it via:
 
             $ pip install numpy
-            """), file=sys.stderr)
+            """
+        ),
+        file=sys.stderr,
+    )
     exit(1)
 
 import numpy
 
 setup(
-    name='bplda',
-    version='0.0.1',
-    license='GPL-v3',
-    description='Latent Dirichlet Allocation with loopy belief propagation',
-    long_description='%s\n%s' % (
-        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
-        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    name="bplda",
+    version="0.0.1",
+    license="GPL-v3",
+    description="Latent Dirichlet Allocation with loopy belief propagation",
+    long_description="%s\n%s"
+    % (
+        re.compile("^.. start-badges.*^.. end-badges", re.M | re.S).sub(
+            "", read("README.rst")
+        ),
+        re.sub(":[a-z]+:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst")),
     ),
-    long_description_content_type='text/x-rst',
-    author='Wolfgang Kopp',
-    author_email='wolfgang.kopp@mdc-berlin.de',
-    url='https://github.com/wkopp/bplda',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    long_description_content_type="text/x-rst",
+    author="Wolfgang Kopp",
+    author_email="wolfgang.kopp@mdc-berlin.de",
+    url="https://github.com/wkopp/bplda",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Utilities',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
     ],
     #    project_urls={
     #        'Documentation': 'https://bplda.readthedocs.io/',
@@ -96,15 +105,16 @@ setup(
     #        'Issue Tracker': 'https://github.com/wkopp/bplda/issues',
     #    },
     keywords=[
-        'Latent Dirichlet Allocation', 'Belief propagation',
+        "Latent Dirichlet Allocation",
+        "Belief propagation",
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=[
-        'numpy',
-        'scikit-learn',
-        'scipy',
-        'tqdm'
+        "numpy",
+        "scikit-learn",
+        "scipy",
+        "tqdm"
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
     extras_require={
@@ -112,18 +122,14 @@ setup(
         #   'rst': ['docutils>=0.11'],
         #   ':python_version=="2.6"': ['argparse'],
     },
-    setup_requires=[
-        'cython',
-        'numpy'
-    ] if Cython else [
-    ],
+    setup_requires=["cython", "numpy"] if Cython else [],
     ext_modules=[
         Extension(
-            splitext(relpath(path, 'src').replace(os.sep, '.'))[0],
+            splitext(relpath(path, "src").replace(os.sep, "."))[0],
             sources=[path],
-            include_dirs=[dirname(path), numpy.get_include()]
+            include_dirs=[dirname(path), numpy.get_include()],
         )
-        for root, _, _ in os.walk('src')
-        for path in glob(join(root, '*.pyx' if Cython else '*.c'))
+        for root, _, _ in os.walk("src")
+        for path in glob(join(root, "*.pyx" if Cython else "*.c"))
     ],
 )
